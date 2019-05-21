bf24317767baf527f88d49979151322cdbdddb38
Philip-Owen <31570473+Philip-Owen@users.noreply.github.com>
2019-05-20 20:29:49 -0500

    fixed issue with selecting color buttons during the display of the pattern

--- src/Actions/index.js
+++ src/Actions/index.js
@@ -1,28 +1,30 @@
 export const lightenColor = (store, color) => {
-	const change = document.getElementById(color);
-	switch (color) {
-		case 'red':
-      change.style.backgroundColor = `#ef6262`;
-      change.style.borderRight = 'black 8px solid';
-      change.style.borderBottom = 'black 8px solid';
-			break;
-		case 'blue':
-      change.style.backgroundColor = `#6a6aff`;
-      change.style.borderLeft = 'black 8px solid';
-      change.style.borderBottom = 'black 8px solid';
-			break;
-		case 'green':
-      change.style.backgroundColor = `#57b357`;
-      change.style.borderRight = 'black 8px solid';
-      change.style.borderTop = 'black 8px solid';
-			break;
-		case 'yellow':
-      change.style.backgroundColor = '#FFFFBB';
-      change.style.borderLeft = 'black 8px solid';
-      change.style.borderTop = 'black 8px solid';
-			break;
-		default:
-			break;
+	if (store.state.gameStarted) {
+		const change = document.getElementById(color);
+		switch (color) {
+			case 'red':
+				change.style.backgroundColor = `#ef6262`;
+				change.style.borderRight = 'black 8px solid';
+				change.style.borderBottom = 'black 8px solid';
+				break;
+			case 'blue':
+				change.style.backgroundColor = `#6a6aff`;
+				change.style.borderLeft = 'black 8px solid';
+				change.style.borderBottom = 'black 8px solid';
+				break;
+			case 'green':
+				change.style.backgroundColor = `#57b357`;
+				change.style.borderRight = 'black 8px solid';
+				change.style.borderTop = 'black 8px solid';
+				break;
+			case 'yellow':
+				change.style.backgroundColor = '#FFFFBB';
+				change.style.borderLeft = 'black 8px solid';
+				change.style.borderTop = 'black 8px solid';
+				break;
+			default:
+				break;
+		}
 	}
 };
 
@@ -30,8 +32,8 @@ export const darkenColor = (store, color, timer) => {
 	setTimeout(
 		function(color) {
 			const change = document.getElementById(color);
-      change.style.backgroundColor = color;
-      change.style.border = 'black 10px solid';
+			change.style.backgroundColor = color;
+			change.style.border = 'black 10px solid';
 		},
 		timer,
 		color
@@ -56,11 +58,9 @@ const createPattern = store => {
 export const getUserPattern = (store, color) => {
 	const { gameStarted, pattern, userCounter, counter } = store.state;
 	if (!gameStarted) {
-		console.log('game not started');
 		return;
 	}
 	if (color === pattern[userCounter]) {
-		console.log('Correct');
 		const newUserCounter = userCounter + 1;
 		if (newUserCounter === counter) {
 			store.setState({ userCounter: 0 });
@@ -69,7 +69,6 @@ export const getUserPattern = (store, color) => {
 			store.setState({ userCounter: newUserCounter });
 		}
 	} else {
-		console.log('Wrong!');
 		endGame(store);
 	}
 };
@@ -82,12 +81,10 @@ const colorTimer = store => {
 	const { pattern } = store.state;
 	let i = 0;
 	let len = pattern.length;
-	console.log(i, len, pattern);
 	setInterval(function() {
 		if (i === len) {
 			clearInterval(this);
 		} else {
-			console.log(pattern[i]);
 			lightenColor(store, pattern[i]);
 			darkenColor(store, pattern[i], 300);
 			i++;
@@ -103,7 +100,6 @@ const continueGame = store => {
 };
 
 const endGame = store => {
-	console.log('Game Over!');
 	const pattern = [];
 	const counter = 0;
 	const userCounter = 0;
